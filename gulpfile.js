7139518c364c48f64f4c4ec2a45b5994ea34c4e2
DESKTOP-FIHAS8K\User <andrescimma1@gmail.com>
2021-01-20 02:26:04 -0300

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,15 @@ const notify = require('gulp-notify');
 const webp = require('gulp-webp');
 const concat = require('gulp-concat');
 
-//Función que compila SASS
+// Utilidades CSS
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Utilidades JS
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
 
 const paths =
 {
@@ -17,29 +25,21 @@ const paths =
 function css()
 {
     return src(paths.scss)
-        .pipe(sass(
-            {
-                outputStyle: 'expanded'
-            }
-        ))
-        .pipe(dest('./build/css'))
-}
-
-function minificarcss()
-{
-    return src(paths.scss)
-        .pipe(sass(
-            {
-                outputStyle: 'compressed'
-            }
-        ))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss(autoprefixer(), cssnano()))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/css'))
 }
 
 function javascript()
 {
     return src(paths.js)
+        .pipe(sourcemaps.init())
         .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({suffix: '.min'}))
         .pipe(dest('./build/js'))
 }
 
@@ -67,7 +67,6 @@ function watchArchivos()
 }
 
 exports.css = css;
-exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 
